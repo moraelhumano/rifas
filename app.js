352dc0e62ba18574dd04f5906c090531c447c0ee
moraelhumano <moraelhumano@gmail.com>
2016-11-13 16:12:33 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+activeController();
+
+
+function activeController(){
+  var database = firebase.database();
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAeJKDVKWYKjZcBffFq6axiqEU4iNupVIo",
+    authDomain: "rifas-ce773.firebaseapp.com",
+    databaseURL: "https://rifas-ce773.firebaseio.com/",
+    storageBucket: "rifas-ce773.appspot.com",
+    messagingSenderId: "155210069413"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+}
+
+
+  
+
