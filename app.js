04d2b108fe1bc1c647092cfabf466a11cd941dee
moraelhumano <moraelhumano@gmail.com>
2016-11-13 18:39:52 -0600

    rifas firebase

--- app.js
+++ app.js
@@ -14,13 +14,34 @@ function activeController(){
   };
   firebase.initializeApp(config);
 
-
-
-
-
-
-
 }
+
+
+
+  $(document).ready(function(){
+
+  var ref = new Firebase('https://rifas-ce773.firebaseio.com/');
+
+  $('#enviar').on('click',function(){
+
+     
+    ref.push({
+      message:$('#name').val()
+    });
+
+    
+  });
+
+  ref.on('child_added',function(snapshot){
+      
+      var newText = snapshot.val();
+
+      $('#texto').append('<p>' + newText.message + '</p>');
+    });
+
+
+  });
+
 
 
   
